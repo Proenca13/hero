47e349a3dcb12490fe7e84047ed73b3deb88ffa5
Henrique <henriqueproenca2004@hotmail.com>
2023-10-03 19:22:55 +0100

    second changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("java")
+    id("application")
+}
+application {
+    mainClass.set("Application")
 }
 
 group = "org.example"
@@ -12,6 +16,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.googlecode.lanterna:lanterna:3.1.1")
 }
 
 tasks.test {
